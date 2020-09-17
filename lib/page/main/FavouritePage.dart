cb0d5ae4c68bb0f9f9a70229c01beb870b534b19
gjh <2912842867@qq.com>
2020-09-17 18:08:59 +0800

    commit

---
+++ lib/page/main/FavouritePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+class FavouritePage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return StateFavouritePage();
+  }
+
+
+}
+
+class StateFavouritePage extends State<FavouritePage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      alignment: Alignment.center,
+      child: Text("关注"),
+    );
+  }
+
+}
--- lib/page/main/NewsPage.dart
+++ lib/page/main/NewsPage.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wzryzs/page/main/FavouritePage.dart';
 
 class NewsPage extends StatefulWidget {
   @override
@@ -12,25 +13,48 @@ class NewsPage extends StatefulWidget {
   }
 }
 
-class StateNewsPage extends State<NewsPage> with TickerProviderStateMixin{
+class StateNewsPage extends State<NewsPage> with TickerProviderStateMixin {
+  double offset = 60;
+  double moveDx = 0;
 
-  double offset =0;
+  PageController pageController = new PageController(initialPage: 1);
+
+  int activePosintion = 1;
 
   NewsPageView newsPageView = NewsPageView();
 
+  List<Widget> pages = [FavouritePage(), NewsPageView()];
+
   Animation animation;
   AnimationController animationController;
+
+  double startDx = 0;
+  double stopDx = 0;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    animationController = AnimationController(duration: Duration(milliseconds: 200),vsync: this);
-
+    pageController.addListener(() {
+      offset = pageController.offset / MediaQuery.of(context).size.width * 60;
+      setState(() {});
+    });
+    animationController =
+        AnimationController(duration: Duration(milliseconds: 200), vsync: this);
+    animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        if (offset == 0) {
+          activePosintion = 1;
+        } else if (offset == 60) {
+          activePosintion = 0;
+        }
+        setState(() {});
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.white,
@@ -47,28 +71,46 @@ class StateNewsPage extends State<NewsPage> with TickerProviderStateMixin{
                 Row(
                   children: [
                     Transform.scale(
-                      scale: .75,
-                      child: Container(
-                        width: 60,
-                        alignment: Alignment.center,
-                        child: Text(
-                          "关注",
-                          style: TextStyle(
-                              decoration: TextDecoration.none,
-                              color: Colors.black),
+                      scale: activePosintion == 0 ? 1 : .75,
+                      child: GestureDetector(
+                        onTap: () {
+                          pageController.jumpToPage(0);
+                          setState(() {
+                            activePosintion = 0;
+                            offset = 0;
+                          });
+                        },
+                        child: Container(
+                          width: 60,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "关注",
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                color: Colors.black),
+                          ),
                         ),
                       ),
                     ),
                     Transform.scale(
-                      scale: 1,
-                      child: Container(
-                        width: 60,
-                        alignment: Alignment.center,
-                        child: Text(
-                          "推荐",
-                          style: TextStyle(
-                              decoration: TextDecoration.none,
-                              color: Colors.black),
+                      scale: activePosintion == 1 ? 1 : .75,
+                      child: GestureDetector(
+                        onTap: () {
+                          pageController.jumpToPage(1);
+                          setState(() {
+                            activePosintion = 1;
+                            offset = 60;
+                          });
+                        },
+                        child: Container(
+                          width: 60,
+                          alignment: Alignment.center,
+                          child: Text(
+                            "推荐",
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                color: Colors.black),
+                          ),
                         ),
                       ),
                     )
@@ -100,7 +142,7 @@ class StateNewsPage extends State<NewsPage> with TickerProviderStateMixin{
               height: 8,
             ),
             Transform.translate(
-              offset: Offset(82-offset, 0),
+              offset: Offset(offset + 22, 0),
               child: Container(
                 alignment: Alignment.centerLeft,
                 width: 16,
@@ -113,51 +155,107 @@ class StateNewsPage extends State<NewsPage> with TickerProviderStateMixin{
           ],
         ),
       ),
-      body: GestureDetector(
-        onTap: (){
-          print("333333");
-        },
-        onHorizontalDragStart: (event){
-
-        },
-        onHorizontalDragUpdate: (event){
-          offset+=event.delta.dx/MediaQuery.of(context).size.width*60;
-          setState(() {
-
-          });
-          print("1111:"+event.delta.dx.toString());
-        },
-        onHorizontalDragEnd: (event){
-          offset = offset.abs();
-          if(offset>=20){
-            animation = Tween<double>(begin: offset,end: 60).animate(animationController)..addListener(() {
-              setState(() {
-                offset = animation.value;
-              });
-            });
-            animationController.reset();
-            animationController.forward();
-          }else{
-            animation = Tween<double>(begin: offset,end: 0).animate(animationController)..addListener(() {
-              setState(() {
-                offset = animation.value;
-              });
-            });
-            animationController.reset();
-            animationController.forward();
-          }
-        },
-        child: AbsorbPointer(
-          absorbing: newsPageView._avtivePostionToOutside==0,
-          child: newsPageView,
-        ),
-      ),
+      body: PageView.builder(
+          controller: pageController,
+          itemCount: pages.length,
+          itemBuilder: (context, index) {
+            return pages[index];
+          }),
+//      body: Listener(
+////        onPointerDown: (event){
+////          startDx = event.localPosition.dx;
+////        },
+////        onPointerMove: (event){
+////          stopDx = event.localPosition.dx;
+////          moveDx = event.delta.dx;
+//////          print(moveDx);
+////          setState(() {
+////
+////          });
+////          if(newsPageView.avtivePostionToOutside!=0&&activePosintion==1){
+////            return;
+////          }
+////          print("offset:"+offset.toString());
+////
+////
+////          offset+=event.delta.dx/MediaQuery.of(context).size.width*60;
+////          if(offset>=60){
+////            offset=60;
+////            setState(() {
+////
+////            });
+////            return;
+////          }
+////          if(offset<=0){
+////            offset = 0 ;
+////            setState(() {
+////
+////            });
+////            return;
+////          }
+//////          print("1111:"+event.delta.dx.toString());
+////
+////        },
+////        onPointerUp: (event){
+////          print("new_ac:"+offset.toString());
+////          if(newsPageView.avtivePostionToOutside!=0){
+////            return;
+////          }
+////          double offsetDx = stopDx-startDx;
+////          print("dddd:"+offsetDx.toString());
+////          if(offsetDx>0){//从左向右滑动
+////            offsetDx = offsetDx.abs();
+////            if(offsetDx>50){
+////              animation = Tween<double>(begin: offset,end: 60).animate(animationController)..addListener(() {
+////                setState(() {
+////                  offset = animation.value;
+////                });
+////              });
+////              animationController.reset();
+////              animationController.forward();
+////            }else{
+////              animation = Tween<double>(begin: offset,end: 0).animate(animationController)..addListener(() {
+////                setState(() {
+////                  offset = animation.value;
+////                });
+////              });
+////              animationController.reset();
+////              animationController.forward();
+////            }
+////          }else{//从右向左滑动
+////            offsetDx = offsetDx.abs();
+////            if(activePosintion==1){
+////              return;
+////            }
+////            if(offsetDx>50){
+////              animation = Tween<double>(begin: offset,end: 0).animate(animationController)..addListener(() {
+////                setState(() {
+////                  offset = animation.value;
+////                });
+////              });
+////              animationController.reset();
+////              animationController.forward();
+////            }else{
+////              animation = Tween<double>(begin: offset,end: 60).animate(animationController)..addListener(() {
+////                setState(() {
+////                  offset = animation.value;
+////                });
+////              });
+////              animationController.reset();
+////              animationController.forward();
+////            }
+////          }
+////        },
+////        child: AbsorbPointer(
+////          absorbing: newsPageView.avtivePostionToOutside==0&&(activePosintion==1?moveDx>0:true)&&activePosintion==0,
+////          child: activePosintion==0?FavouritePage():newsPageView,
+////        ),
+//      ),
     );
   }
 }
 
 class NewsPageView extends StatefulWidget {
-
   int _avtivePostionToOutside = 0;
 
   int get avtivePostionToOutside => _avtivePostionToOutside;
@@ -202,10 +300,16 @@ class StateNewsPageView extends State<NewsPageView>
   Animation animation;
   double bgOffset = 0;
 
+  double moveDx = 0;
+  double offset = 0;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    pageController.addListener(() {
+//      print("pc:" + pageController.offset.toString());
+    });
     animationController =
         AnimationController(duration: Duration(milliseconds: 300), vsync: this);
     scrollController
@@ -222,7 +326,7 @@ class StateNewsPageView extends State<NewsPageView>
       } else {
         titleWidth = getOffset(activePosition) - getOffset(activePosition - 1);
       }
-      print("titleWidth:" + titleWidth.toString());
+//      print("titleWidth:" + titleWidth.toString());
       setState(() {
         bgOffset = titleWidth *
                 (pageController.offset % MediaQuery.of(context).size.width) /
@@ -230,7 +334,7 @@ class StateNewsPageView extends State<NewsPageView>
             getOffset(
                 (pageController.offset ~/ MediaQuery.of(context).size.width));
       });
-      print("page:" + pageController.offset.toString());
+//      print("page:" + pageController.offset.toString());
     });
   }
 
@@ -343,8 +447,63 @@ class StateNewsPageView extends State<NewsPageView>
         Expanded(
 //          width: MediaQuery.of(context).size.width,width
 //          height:MediaQuery.of(context).size.height,
-          child: PageView.builder(
-              onPageChanged: (position) {
+          child: Listener(
+            onPointerDown: (detail) {
+              offset = 0;
+            },
+            onPointerMove: (detail) {
+              offset += detail.delta.dx;
+              moveDx = detail.delta.dx;
+              setState(() {});
+            },
+            onPointerUp: (detail) {
+              if (offset < 0) {
+                offset = offset.abs();
+                if (offset % MediaQuery.of(context).size.width >
+                    MediaQuery.of(context).size.width / 5) {
+                  pageController.animateToPage(activePosition + 1,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeOut);
+                } else {
+                  pageController.animateToPage(activePosition,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeOut);
+                }
+              } else {
+                offset = offset.abs();
+                if (offset % MediaQuery.of(context).size.width >
+                    MediaQuery.of(context).size.width / 5) {
+                  pageController.animateToPage(activePosition - 1,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeOut);
+                } else {
+                  pageController.animateToPage(activePosition,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeOut);
+                }
+              }
+
+            },
+            child: PageView.builder(
+                physics: activePosition == 0 && moveDx > 0
+                    ? NeverScrollableScrollPhysics()
+                    : null,
+                pageSnapping: false,
+                onPageChanged: (position) {
+                  print("onPageChanged:"+bgOffset.toString());
+                  print("onPageChanged:"+(getOffset(position)/2).toString());
+                  print("onPageChanged:"+(MediaQuery.of(context).size.width / 2).toString());
+                  double scroll =  (bgOffset +getOffset(position-1)/2)>
+                      (MediaQuery.of(context).size.width / 2)
+                      ? (bgOffset+getOffset(position-1)/2  -
+                      MediaQuery.of(context).size.width / 2)
+                      : 0;
+                  print("onPageChanged:"+scroll.toString());
+                  print("onPageChanged:"+scrollController.offset.toString());
+                  scrollController.animateTo(
+                      scroll-scrollController.offset,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.easeOut);
 //                animation = Tween<double>(
 //                    begin: getOffset(lastActivePosition),
 //                    end: getOffset(position))
@@ -362,20 +521,22 @@ class StateNewsPageView extends State<NewsPageView>
 //                  activePosition = position;
 //                  setState(() {});
 //                });
-                setState(() {
-                  lastActivePosition = activePosition;
-                  activePosition = position;
-                  widget._avtivePostionToOutside = position;
-                });
-              },
-              controller: pageController,
-              itemCount: titleList.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  alignment: Alignment.center,
-                  child: Text(titleList[index]),
-                );
-              }),
+                  print("onPageChanged:"+scrollController.offset.toString());
+                  setState(() {
+                    lastActivePosition = activePosition;
+                    activePosition = position;
+                    widget._avtivePostionToOutside = position;
+                  });
+                },
+                controller: pageController,
+                itemCount: titleList.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    alignment: Alignment.center,
+                    child: Text(titleList[index]),
+                  );
+                }),
+          ),
         )
       ],
     );
